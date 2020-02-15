017e1452646bcba5c14b9220c2903ffefa56d0e0
Liljan <marcus.t.lilja@gmail.com>
2020-02-15 22:57:03 +0100

    Customize character look.

---
+++ SurvivalShooter/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer m_Head;
+    [SerializeField] private SpriteRenderer m_Body;
+    [SerializeField] private SpriteRenderer m_Legs;
+    [SerializeField] private SpriteRenderer m_Weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(m_Head, "Head missing from player prefab.");
+        Debug.Assert(m_Body, "Body missing from player prefab.");
+        Debug.Assert(m_Legs, "Legs missing from player prefab.");
+        Debug.Assert(m_Weapon, "Weapon missing from player prefab.");
+    }
+
+    public void SetHead(Sprite head) { m_Head.sprite = head; }
+    public void SetBody(Sprite body) { m_Body.sprite = body; }
+    public void SetLegs(Sprite legs) { m_Legs.sprite = legs; }
+    public void SetWeapon(Sprite weapon) { m_Weapon.sprite = weapon; }
+}
---
+++ SurvivalShooter/Assets/Scripts/PlayerCustomization.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCustomization : MonoBehaviour
+{
+    [SerializeField] private Player m_Player;
+
+    [SerializeField] private Sprite[] m_Heads;
+    [SerializeField] private Sprite[] m_Bodies;
+    [SerializeField] private Sprite[] m_Legs;
+    [SerializeField] private Sprite[] m_Weapons;
+
+    private void OnEnable()
+    {
+        m_Player.SetHead(m_Heads[0]);
+        m_Player.SetBody(m_Bodies[0]);
+        m_Player.SetLegs(m_Legs[0]);
+        m_Player.SetWeapon(m_Weapons[0]);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp("r"))
+        {
+            m_Player.SetHead(m_Heads[Random.Range(0, m_Heads.Length)]);   
+            m_Player.SetBody(m_Bodies[Random.Range(0, m_Bodies.Length)]);
+            //m_Player.SetLegs(m_Legs[Random.Range(0, m_Legs.Length)]);
+            m_Player.SetWeapon(m_Weapons[Random.Range(0, m_Weapons.Length)]);
+        }
+    }
+}
