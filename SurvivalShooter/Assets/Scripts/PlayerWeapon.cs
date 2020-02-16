06428c1dec9aa96a407e11c2d3478e933e6d0e8d
Liljan <marcus.t.lilja@gmail.com>
2020-02-16 20:32:51 +0100

    Move reticle around.

--- SurvivalShooter/Assets/Scripts/PlayerWeapon.cs
+++ SurvivalShooter/Assets/Scripts/PlayerWeapon.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
 {
+    [SerializeField] private GameObject m_Reticle;
+
+    // Temporary
+    [SerializeField] private float m_AimRadius;
+
+    private void OnEnable()
+    {
+        Debug.Assert(m_Reticle, "No reticle gameobjects attached to the player weapon");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleAim();
+    }
+
+    private void HandleAim()
+    {
+        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+
+        Vector2 center = gameObject.transform.position;
+        Vector2 direction = worldPoint - center;
+        direction.Normalize();
+
+        Vector2 aimPoint = center + direction * m_AimRadius;
+        float reticleZ = m_Reticle.transform.position.z;
+
+        m_Reticle.transform.position = new Vector3(aimPoint.x, aimPoint.y, reticleZ);     
     }
 }
